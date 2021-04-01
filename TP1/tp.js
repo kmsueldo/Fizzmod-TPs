d1eaa7821efc6830710d82f1c2b9717b6620c60a
kmsueldo <kmsueldo@hotmail.com>
2021-04-01 12:28:10 -0300

    Se corre test sobre los primeros 2 ejercicios, test ok

--- TP1/tp.js
+++ TP1/tp.js
@@ -9,7 +9,6 @@ function contarVocales(text) {
     let textLowerCase = text.toLowerCase()
     let arrayText = textLowerCase.split('')
     let letterCount = 0
-
     for (const letter of arrayText) {
         if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
           letterCount++
@@ -18,7 +17,8 @@ function contarVocales(text) {
     console.log(`La cantidad de vocales sin acento que tiene el texto:  "${text}"   es:   ${letterCount}`);
     return letterCount
   }else {
-    return -1
+    letterCount = -1
+    return letterCount
   }
   
 }
@@ -30,17 +30,17 @@ Esta función devolverá un string con la url del repo.
 */
 function urlRepo() {
   const url = 'https://github.com/kmsueldo/Fizzmod-TPs.git'
-
   return url
 }
 
 /* 
-Crear una función arrow, que devuelva una clase en ES6 que contendrá dos métodos llamados contadorPalabras y hayNumeros. La clase recibirá un texto que se guardará en una propiedad llamada texto. contadorPalabras retornará la cantidad de palabras encontradas en la propiedad texto y hayNumeros devolverá true en caso de que encuentre un número en dicho texto, caso contrario retorna false. En ambos métodos, si el texto no es válido, se devolverá -1
+Crear una función arrow, que devuelva una clase en ES6 que contendrá dos métodos llamados contadorPalabras y hayNumeros. 
+La clase recibirá un texto que se guardará en una propiedad llamada texto. 
+contadorPalabras retornará la cantidad de palabras encontradas en la propiedad texto y hayNumeros devolverá true en caso de que encuentre un número en dicho texto, caso contrario retorna false. En ambos métodos, si el texto no es válido, se devolverá -1
 Crear un propiedad estática contadorInstancias que me indique cuantas instancias hay de esa clase.
 */
 const crearClase = () => {
-  
-  return url
+  return
 }
 
 
@@ -51,8 +51,9 @@ const crearClase = () => {
 
 /* -------------------FUNCION CONTAR VOCALES----------------------- */
 
-let texto = 'hOlA 1 como EstAs?'
+let texto = 1
 contarVocales(texto)
+console.log(contarVocales(texto));
 
 /* -------------------FUNCION URL REPOSITORIO----------------------- */
 
